6d80bfafc5c8837f829b1831ca272fd774214e19
Akshay Mishra <mishra.akshay91@gmail.com>
2018-05-06 23:06:19 -0700

    integration with the feedback app

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -6,8 +6,8 @@ module.exports = survey => {
 			<body>
 				<div style="text-align : center;">
 					<h3>I'd like your input</h3>
-					<p>	Please answer the following question: </p>
-					<p> ${survey.body} </p>
+					<p>	Glad that you used our service. Was our response on below tweet helpful? </p>
+					<p> ${survey.title} </p>
 					<div>
 						<a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes"> Yes </a>						
 					</div>
